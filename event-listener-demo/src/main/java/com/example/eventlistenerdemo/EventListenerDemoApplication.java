f63db72e2f1abd25fdd5776a37f1fc367f458a5b
viveksoni100 <viveksoni100@gmail.com>
2024-02-18 02:13:14 +0530

    springboot project initiated

---
+++ event-listener-demo/src/main/java/com/example/eventlistenerdemo/EventListenerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventlistenerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventListenerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventListenerDemoApplication.class, args);
+	}
+
+}
---
+++ event-listener-demo/src/test/java/com/example/eventlistenerdemo/EventListenerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventlistenerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventListenerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
