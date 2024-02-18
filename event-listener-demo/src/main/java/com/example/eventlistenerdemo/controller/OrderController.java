b2d5375948ed88da3299474a9e4a953fd60684f8
viveksoni100 <viveksoni100@gmail.com>
2024-02-18 13:43:15 +0530

    ice cream order form completed

--- event-listener-demo/src/main/java/com/example/eventlistenerdemo/controller/OrderController.java
+++ event-listener-demo/src/main/java/com/example/eventlistenerdemo/controller/OrderController.java
@@ -1,15 +1,25 @@
 package com.example.eventlistenerdemo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OrderController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public ModelAndView iceCreamOrderForm() {
-        return new ModelAndView("order-page");
+        ModelAndView model = new ModelAndView("order-page");
+        model.addObject("orderplaced", Boolean.FALSE);
+        return model;
+    }
+
+    @PostMapping("/placeorder")
+    public ModelAndView placeAnOrder() {
+        ModelAndView model = new ModelAndView("order-page");
+        model.addObject("orderplaced", Boolean.TRUE);
+        return model;
     }
 
 }
